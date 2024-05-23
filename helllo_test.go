02bf2586e3b09295367559c5cfde0ce31783c099
Makism-Gr <maksim.g.grishchenko@gmail.com>
2024-05-23 22:12:21 +0300

    test: add test cases

--- helllo_test.go
+++ helllo_test.go
@@ -3,10 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("someName")
-	want := "Hello someName"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("name")
+		want := "Hello, name"
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+	t.Run("say hello default when no arg is provided", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, default"
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
 }

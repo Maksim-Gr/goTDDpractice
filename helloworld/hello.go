781045d86a57828a25edfc8bb08f306b4d91ed75
Makism-Gr <maksim.g.grishchenko@gmail.com>
2024-05-24 15:39:20 +0300

    chore: add test and examples

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -1,6 +1,4 @@
-package main
-
-import "fmt"
+package helloworld
 
 const englishHelloPrefix = "Hello, "
 const spanish = "Spanish"
@@ -26,8 +24,4 @@ func greetingPrefix(language string) (prefix string) {
 		prefix = englishHelloPrefix
 	}
 	return
-}
-
-func main() {
-	fmt.Println(Hello("someName", ""))
 }

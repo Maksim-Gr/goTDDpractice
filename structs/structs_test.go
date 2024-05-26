2c5467a050b452b8b0651c3b14aed30da0ae6da1
Makism-Gr <maksim.g.grishchenko@gmail.com>
2024-05-26 22:09:05 +0300

    chore_tests: play around with structs

---
+++ structs/structs_test.go
@@ -0,0 +1,23 @@
+package structs
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rect := Rectangle{Width: 10.0, Height: 10.0}
+	got := Perimeter(rect)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f, want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	rect := Rectangle{Width: 12.0, Height: 6.0}
+	got := Area(rect)
+	want := 72.0
+
+	if got != want {
+		t.Errorf("got %.2f, want %.2f", got, want)
+	}
+}

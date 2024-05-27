6fc32962105130ad05989d0f61139829edf8f8f3
Makism-Gr <maksim.g.grishchenko@gmail.com>
2024-05-27 20:37:53 +0300

    test: check out table driven tests

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -15,22 +15,18 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{Width: 12.0, Height: 6.0}, 72.0},
+		{Circle{Radius: 10.0}, 314.1592653589793},
+	}
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2f, want %.2f", got, tt.want)
 		}
 	}
 
-	t.Run("rectangle", func(t *testing.T) {
-		rectangle := Rectangle{Width: 12.0, Height: 6.0}
-		checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("circle", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
 }

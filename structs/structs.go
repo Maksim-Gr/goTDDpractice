d0f6acb107ed28b646bd63b1b5b093a5f25e4853
Makism-Gr <maksim.g.grishchenko@gmail.com>
2024-05-27 20:30:31 +0300

    mintest: bare minimum test for methods

--- structs/structs.go
+++ structs/structs.go
@@ -4,6 +4,18 @@ type Rectangle struct {
 	Width  float64
 	Height float64
 }
+type Circle struct {
+	Radius float64
+}
+
+func (r Rectangle) Area() float64 {
+	return 0
+
+}
+
+func (c Circle) Area() float64 {
+	return 0
+}
 
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Height + rectangle.Width)
--- structs/structs_test.go
+++ structs/structs_test.go
@@ -1,6 +1,8 @@
 package structs
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPerimeter(t *testing.T) {
 	rect := Rectangle{Width: 10.0, Height: 10.0}
@@ -13,11 +15,21 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rect := Rectangle{Width: 12.0, Height: 6.0}
-	got := Area(rect)
-	want := 72.0
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{Width: 12.0, Height: 6.0}
+		got := rectangle.Area()
+		want := 72.0
+		if got != want {
+			t.Errorf("got %.2f, want %.2f", got, want)
+		}
+	})
 
-	if got != want {
-		t.Errorf("got %.2f, want %.2f", got, want)
-	}
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+		if got != want {
+			t.Errorf("got %.2f, want %.2f", got, want)
+		}
+	})
 }

6eabc922e7d7104ba1a837be94325499dca21998
Makism-Gr <maksim.g.grishchenko@gmail.com>
2024-05-28 20:45:15 +0300

    test: impl stringer interface

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -1,8 +1,16 @@
 package pointers
 
+import (
+	"fmt"
+)
+
 type Coin int
 type Wallet struct {
 	balance Coin
+}
+
+func (c Coin) String() string {
+	return fmt.Sprintf("%d COIN", c)
 }
 
 func (w *Wallet) Deposit(amount Coin) {

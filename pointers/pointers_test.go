3a241e02c4dc349a0bc7ac1cac1e56298342e0d2
Makism-Gr <maksim.g.grishchenko@gmail.com>
2024-05-30 20:53:11 +0300

    refactor: add helper method for tests

--- pointers/pointers_test.go
+++ pointers/pointers_test.go
@@ -3,24 +3,24 @@ package pointers
 import "testing"
 
 func TestWallet(t *testing.T) {
+	assertBalance := func(t testing.TB, wallet Wallet, want Coin) {
+		t.Helper()
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Coin(10))
-		got := wallet.Balance()
-		want := Coin(10)
-		if got != want {
-			t.Errorf("got %d, want %d", got, want)
-		}
+		assertBalance(t, wallet, Coin(10))
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
-		wallet := Wallet{}
+		wallet := Wallet{balance: Coin(20)}
 		wallet.Withdraw(Coin(10))
-		got := wallet.Balance()
-		want := Coin(10)
-
-		if got != want {
-			t.Errorf("got %d, want %d", got, want)
-		}
+		assertBalance(t, wallet, Coin(10))
 	})
 }

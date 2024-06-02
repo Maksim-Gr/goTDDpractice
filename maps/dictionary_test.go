d8db9eb595539aa01c9e6aedc7303a3c87b900f0
Makism-Gr <maksim.g.grishchenko@gmail.com>
2024-06-02 20:34:26 +0300

    test: refactor with helper func

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -7,6 +7,11 @@ func TestSearch(t *testing.T) {
 
 	got := Search(dictionary, "test")
 	want := "this is a test value"
+	AssertStringValues(t, got, want)
+}
+
+func AssertStringValues(t testing.TB, got, want string) {
+	t.Helper()
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
